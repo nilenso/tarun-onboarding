f89bac9a5b67f5f9479cd713369ceb7dae828fc5
Tarun <tarun@nilenso.com>
2024-01-19 16:15:51 +0530

    Init TracMe app

---
+++ TracMe/src/main/java/com/nilenso/TracMe/TracMeApplication.java
@@ -0,0 +1,13 @@
+package com.nilenso.TracMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracMeApplication.class, args);
+	}
+
+}
