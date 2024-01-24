6d65ad924cc660242b4c7157a1088610526298fb
Tarun <tarun@nilenso.com>
2024-01-24 16:55:37 +0530

    Change from Controller to RestController

--- swift-activities/src/main/java/com/nilenso/swiftactivities/controllers/ActivityController.java
+++ swift-activities/src/main/java/com/nilenso/swiftactivities/controllers/ActivityController.java
@@ -2,12 +2,12 @@ package com.nilenso.swiftactivities.controllers;
 
 import com.nilenso.swiftactivities.domain.Activity;
 import com.nilenso.swiftactivities.repositories.ActivityRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/activity")
 public class ActivityController {
     private final ActivityRepository activityRepository;
