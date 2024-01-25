968380b44f614ab56c897c58df277455b05b3927
Tarun <tarun@nilenso.com>
2024-01-25 12:10:51 +0530

    Add test for health check controller

---
+++ swift-activities/src/test/java/com/nilenso/swiftactivities/controllers/HealthCheckControllerTest.java
@@ -0,0 +1,35 @@
+package com.nilenso.swiftactivities.controllers;
+
+import com.nilenso.swiftactivities.services.HealthCheckService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+class HealthCheckControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    HealthCheckService healthCheckService;
+
+    @Test
+    void healthCheck() throws Exception {
+        var response = "OK";
+
+        when(healthCheckService.healthCheck()).thenReturn(response);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/health-check"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(response));
+    }
+}
