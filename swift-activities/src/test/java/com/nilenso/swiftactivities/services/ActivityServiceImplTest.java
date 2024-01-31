2ae731d21f88734292b68859d589f2b70f4da04d
Tarun <tarun@nilenso.com>
2024-01-31 18:28:50 +0530

    Add unit test for end activity service

--- swift-activities/src/test/java/com/nilenso/swiftactivities/services/ActivityServiceImplTest.java
+++ swift-activities/src/test/java/com/nilenso/swiftactivities/services/ActivityServiceImplTest.java
@@ -9,7 +9,10 @@ import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
 
+import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -22,7 +25,9 @@ class ActivityServiceImplTest {
     ActivityRepository activityRepository = mock(ActivityRepository.class);
 
     @Captor
-    ArgumentCaptor<Activity> captor;
+    ArgumentCaptor<Activity> addActivityCaptor, endActivityCaptor;
+    @Captor
+    ArgumentCaptor<UUID> findByIdCaptor;
 
     @Test
     void addActivity() {
@@ -32,14 +37,38 @@ class ActivityServiceImplTest {
         when(activityRepository.save(any())).thenReturn(new Activity());
 
         try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
-           mockedStatic.when(Instant::now).thenReturn(now);
-           activityService.addActivity(startActivityDto);
+            mockedStatic.when(Instant::now).thenReturn(now);
+            activityService.addActivity(startActivityDto);
         }
 
-        Mockito.verify(activityRepository).save(captor.capture());
-        Activity capturedActivity = captor.getValue();
+        Mockito.verify(activityRepository).save(addActivityCaptor.capture());
+        Activity capturedActivity = addActivityCaptor.getValue();
         assertEquals(startActivityDto.type(), capturedActivity.getType());
         assertEquals(startActivityDto.name(), capturedActivity.getName());
         assertEquals(now, capturedActivity.getStartTime());
+    }
+
+    @Test
+    void endActivity() {
+        var now = Instant.now();
+        var activityId = randomUUID();
+
+        when(activityRepository.findById(any())).thenReturn(Optional.of(new Activity()));
+
+        when(activityRepository.save(any())).then(invocationOnMock ->
+                invocationOnMock.getArgument(0));
+
+        try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+            mockedStatic.when(Instant::now).thenReturn(now);
+            activityService.endActivity(activityId);
+        }
+
+        Mockito.verify(activityRepository).findById(findByIdCaptor.capture());
+        UUID capturedActivityId = findByIdCaptor.getValue();
+        assertEquals(activityId, capturedActivityId);
+
+        Mockito.verify(activityRepository).save(endActivityCaptor.capture());
+        Activity capturedActivity = endActivityCaptor.getValue();
+        assertEquals(now, capturedActivity.getEndTime());
     }
 }
